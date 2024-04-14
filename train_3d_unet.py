ed01381ac102ee0bb826670ef70ea7f971754d74
meglaficus <jakobmeglic123@gmail.com>
2024-04-14 11:26:30 +0200

    bugs squashed

--- train_3d_unet.py
+++ train_3d_unet.py
@@ -1,10 +1,11 @@
 from keras.optimizers import Adam
-from keras import backend as K
+from tensorflow.keras import backend as K
 from keras.callbacks import ModelCheckpoint, LearningRateScheduler, CSVLogger
 from keras.models import load_model
 from keras.utils import plot_model
 import numpy as np
 import json
+import tensorflow as tf
 
 from generator.generator_for_3d_unet import MyGenerator
 from build_models.unet_3d import build_unet
@@ -21,20 +22,20 @@ import pytz
 #############
 
 def dc(y_true, y_pred):
-    y_true = K.flatten(y_true)
-    y_pred = K.flatten(y_pred)
+    y_true = K.flatten(tf.cast(y_true, tf.float64))
+    y_pred = K.flatten(tf.cast(y_pred, tf.float64))
     intersection = K.sum(y_true * y_pred)
     return (2.0 * intersection + 1) / (K.sum(y_true) + K.sum(y_pred) + 1)
 
 def se(y_true, y_pred):
-    y_true = K.flatten(y_true)
-    y_pred = K.flatten(y_pred)
+    y_true = K.flatten(tf.cast(y_true, tf.float64))
+    y_pred = K.flatten(tf.cast(y_pred, tf.float64))
     intersection = K.sum(y_true * y_pred)
     return (intersection + 1) / (K.sum(y_true) + 1)
 
 def tversky_08(y_true, y_pred):
-    y_true_pos = K.flatten(y_true)
-    y_pred_pos = K.flatten(y_pred)
+    y_true_pos = K.flatten(tf.cast(y_true, tf.float64))
+    y_pred_pos = K.flatten(tf.cast(y_pred, tf.float64))
     true_pos = K.sum(y_true_pos * y_pred_pos)
     false_neg = K.sum(y_true_pos * (1 - y_pred_pos))
     false_pos = K.sum((1 - y_true_pos) * y_pred_pos)
@@ -66,22 +67,23 @@ def train(train_gen, val_gen, fold, args):
     model = build_unet(input_shape)
 
     model.summary()
-    os.makedirs(os.path.join(args.results_dir, f'fold_{fold}'), exist_ok=True)
     
-    path = os.path.join(args.results_dir, f'fold_{fold}', "model.png")
+    path = os.path.join(args.base_dir, args.results_dir, f'fold_{fold}', "model.png")
+    print(path)
     plot_model(model, to_file=path, show_shapes=True, show_layer_names=True)
 
     model.compile(loss=focal_tversky_loss_08, optimizer=Adam(learning_rate=LR), metrics=[dc, se])
 
+    time = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
+    print(time)
+    
     cs = []
-    csvlog_path = os.path.join(args.results_dir, 'fold_{fold}', "log", args.dt + ".csv")
+    csvlog_path = os.path.join(args.base_dir, args.results_dir, f'fold_{fold}', "log", time + ".csv")
     csv_logger = CSVLogger(csvlog_path)
     cs = cs + [csv_logger]
 
-    time = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
-    print(args.dt)
 
-    model_path = args.results_dir + f'/fold_{fold}' + "/models/" + time + ".h5"
+    model_path = args.results_dir + f'/fold_{fold}' + "/models/" + time + ".keras"
     
     mc = ModelCheckpoint(filepath=model_path, monitor="val_loss", save_best_only=True)
     cs = cs + [mc]
@@ -104,7 +106,7 @@ def train(train_gen, val_gen, fold, args):
     K.clear_session()
 
 
-    model = load_model(args.model_path, custom_objects={"focal_tversky_loss_08": focal_tversky_loss_08, "dc": dc, "se": se, })
+    model = load_model(args.model_path, custom_objects={"focal_tversky_loss_08": focal_tversky_loss_08, "dc": dc, "se": se})
     score = model.evaluate_generator(val_gen)
     print(score)
 
@@ -122,7 +124,7 @@ def run(args):
     ## Prepare train_gen and test_gen ##
     ####################################
 
-    dataset_path = os.path.join(args.base, args.dataset_dir)
+    dataset_path = os.path.join(args.base_dir, args.dataset_dir)
     B = args.batch_size
     
     json_path = os.path.join(args.base_dir, args.folds_path)
@@ -132,8 +134,8 @@ def run(args):
     
     # for each of 5 folds in split
     for fold in range (5):
-        train_ids = folds[fold]['train']
-        val_ids = folds[fold]['val']
+        train_ids = folds[str(fold)]['train']
+        val_ids = folds[str(fold)]['val']
         
         train_ct_list = []
         train_lb_list = []
@@ -157,7 +159,12 @@ def run(args):
         train_gen = MyGenerator(train_ct_list, train_lb_list, batch_size=B, flip=1, alpha=0.2, beta=0.3, ws=[2000, 400, 600, 150])
         val_gen = MyGenerator(val_ct_list, val_lb_list, batch_size=B, flip=0, alpha=0, beta=0, ws=[2000, 400, 600, 150])
 
-
+        d = os.path.join(args.base_dir, args.results_dir, f'fold_{fold}')
+        l = [d, d + "/log", d + "/models", d + "/learning_curves", d + "/predicted_images"]
+        for i in l:
+            if not (os.path.exists(i)):
+                os.makedirs(i)
+        
         ###########
         ## Train ##
         ###########
@@ -181,15 +188,9 @@ def get_args():
     parser.add_argument("-dd", "--dataset_dir", type=str, default="scans/")
     parser.add_argument("-rd", "--results_dir", type=str, default="results/")
     parser.add_argument("-fp", "--folds_path", type=str, default="folds.json")
-    parser.add_argument("-b", "--batch_size", type=int, default=6)
+    parser.add_argument("-b", "--batch_size", type=int)
     
     args = parser.parse_args()
-
-    d = args.results_dir
-    l = [d, d + "/log", d + "/models", d + "/learning_curves", d + "/predicted_images"]
-    for i in l:
-        if not (os.path.exists(i)):
-            os.makedirs(i)
 
     return args
 
